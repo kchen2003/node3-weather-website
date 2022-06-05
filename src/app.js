23f17149d588e7191d8ca96a58033697d75f63ca
Kevin Chen <kchen_2003@yahoo.com>
2022-06-05 13:10:33 -0400

    Test git modification

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 // console.log(__dirname)
-// console.log(path.join(__dirname, '../public'))
+//// console.log(path.join(__dirname, '../public'))
 
 const app = express()
 
